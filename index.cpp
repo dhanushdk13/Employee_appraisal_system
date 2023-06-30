f67c267a60c1ce4d17adb1069e4259bc8d6c60f8
Ashwin Ragupathy <ashwin.ragupathy@gmail.com>
2023-06-30 23:05:08 +0530

    second major commit

--- index.cpp
+++ index.cpp
@@ -1,39 +1,108 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <sstream>
+
 using namespace std;
 
 //Tech Lead Login Function
-/* 
+
+
+
+
 struct TechLead {
     string employeeId;
     string name;
     string password;
 };
 
+string trim(const string& str) {
+    size_t start = str.find_first_not_of(" \t\r\n");
+    size_t end = str.find_last_not_of(" \t\r\n");
+    if (start == string::npos || end == string::npos) {
+        return "";
+    }
+    return str.substr(start, end - start + 1);
+}
+
+// Function to check Tech Lead login credentials
 bool loginTechLead(const string& employeeId, const string& password) {
     ifstream file("data/tech_leads.txt");
     if (!file) {
-        cout << "Error opening file. Unable to perform login." << endl;
+        cout << "Error opening tech_leads.txt file. Unable to perform login." << endl;
         return false;
     }
-    
+
     string line;
     while (getline(file, line)) {
-        size_t pos = line.find(' ');
-        if (pos == string::npos) {
-            pos = line.find(',');
-        }
-        
-        string id = line.substr(0, pos);
-        string pass = line.substr(pos + 1);
-        
+        if (line.empty()) continue;
+
+        stringstream ss(line);
+        string id, firstName, pass;
+        ss >> id >> firstName >> pass;
+
         if (id == employeeId && pass == password) {
             file.close();
             return true;
         }
     }
-} */
+
+    file.close();
+    return false;
+}
+
+// Function to check Manager login credentials
+bool loginManager(const string& employeeId, const string& password) {
+    ifstream file("data/manager.txt");
+    if (!file) {
+        cout << "Error opening managers.txt file. Unable to perform login." << endl;
+        return false;
+    }
+
+    string line;
+    while (getline(file, line)) {
+        if (line.empty()) continue;
+
+        stringstream ss(line);
+        string id, firstName, pass;
+        ss >> id >> firstName >> pass;
+
+        if (id == employeeId && pass == password) {
+            file.close();
+            return true;
+        }
+    }
+
+    file.close();
+    return false;
+}
+
+bool loginHR(const string& employeeId, const string& password) {
+    ifstream file("data/hr.txt");
+    if (!file) {
+        cout << "Error opening managers.txt file. Unable to perform login." << endl;
+        return false;
+    }
+
+    string line;
+    while (getline(file, line)) {
+        if (line.empty()) continue;
+
+        stringstream ss(line);
+        string id, firstName, pass;
+        ss >> id >> firstName >> pass;
+
+        if (id == employeeId && pass == password) {
+            file.close();
+            return true;
+        }
+    }
+
+    file.close();
+    return false;
+}
+
+
 
 void displayMainMenu() {
     cout << "----- Employee Appraisal System -----" << endl;
@@ -76,27 +145,31 @@ void displayHumanResourcesSubMenu() {
     cout << "Enter your choice: ";
 }
 
-void handleTechLeadSubMenuOption(char subChoice) {
-    switch (subChoice) {
-        case 'a':
-
-            cout << "Rate Yourself selected." << endl;
-            // Add logic to handle Rate Yourself
-            break;
-        case 'b':
-            cout << "View Your Final Ratings and Hike selected." << endl;
-            // Add logic to handle View Your Final Ratings and Hike
-            break;
-        case 'c':
-            cout << "Exiting Tech Lead Menu." << endl;
-            break;
-        default:
-            cout << "Invalid choice. Please try again." << endl;
-            break;
-    }
+void handleTechLeadSubMenu() {
+    char choice;
+    do {
+        displayTechLeadSubMenu();
+        cin >> choice;
+        // Add your code here to handle the Tech Lead submenu options
+        // For example:
+        switch (choice) {
+            case 'a':
+                // Handle Rate Yourself
+                break;
+            case 'b':
+                // Handle View Your Final Ratings and Hike
+                break;
+            case 'c':
+                cout << "Exiting Tech Lead Menu. Goodbye!" << endl;
+                break;
+            default:
+                cout << "Invalid choice. Please try again." << endl;
+                break;
+        }
+    } while (choice != 'c');
 }
 
-void handleEngineeringManagerSubMenuOption(char subChoice) {
+/* void handleEngineeringManagerSubMenuOption(char subChoice) {
     switch (subChoice) {
         case 'a':
             cout << "Tech Lead Ratings selected." << endl;
@@ -117,8 +190,40 @@ void handleEngineeringManagerSubMenuOption(char subChoice) {
             cout << "Invalid choice. Please try again." << endl;
             break;
     }
+} */
+
+
+void handleEMSubMenu() {
+    char choice;
+    do {
+        displayEngineeringManagerSubMenu();
+        cin >> choice;
+        // Add your code here to handle the Tech Lead submenu options
+        // For example:
+        switch (choice) {
+        case 'a':
+            cout << "Tech Lead Ratings selected." << endl;
+            // Add logic to handle Tech Lead Ratings
+            break;
+        case 'b':
+            cout << "Distribution Chart selected." << endl;
+            // Add logic to handle Distribution Chart
+            break;
+        case 'c':
+            cout << "Decide Tasks for Tech Lead selected." << endl;
+            // Add logic to handle Decide Tasks for Tech Lead
+            break;
+        case 'd':
+            cout << "Exiting Engineering Manager Menu." << endl;
+            break;
+        default:
+            cout << "Invalid choice. Please try again." << endl;
+            break;
+        }
+    } while (choice != 'd');
 }
 
+/* 
 void handleHumanResourcesSubMenuOption(char subChoice) {
     switch (subChoice) {
         case 'a':
@@ -140,11 +245,47 @@ void handleHumanResourcesSubMenuOption(char subChoice) {
             cout << "Invalid choice. Please try again." << endl;
             break;
     }
+} */
+
+
+void handleHRSubMenu() {
+    char choice;
+    do {
+        displayHumanResourcesSubMenu();
+        cin >> choice;
+        // Add your code here to handle the Tech Lead submenu options
+        // For example:
+        switch (choice) {
+        case 'a':
+            cout << "View Self Ratings of Tech Leads selected." << endl;
+            // Add logic to handle View Self Ratings of Tech Leads
+            break;
+        case 'b':
+            cout << "View Ratings Provided by Manager selected." << endl;
+            // Add logic to handle View Ratings Provided by Manager
+            break;
+        case 'c':
+            cout << "Set Final Ratings selected." << endl;
+            // Add logic to handle Set Final Ratings
+            break;
+        case 'd':
+            cout << "Exiting Human Resources Menu." << endl;
+            break;
+        default:
+            cout << "Invalid choice. Please try again." << endl;
+            break;
+        }
+    } while (choice != 'd');
 }
+
+
+
 
 int main() {
     int choice;
     char subChoice;
+    string employeeId;
+    string password;
     
     do {
         displayMainMenu();
@@ -156,23 +297,51 @@ int main() {
                 cin >> subChoice;
                 
                 switch (subChoice) {
-                    case 'a':
-                        displayTechLeadSubMenu();
-                        cin >> subChoice;
-                        handleTechLeadSubMenuOption(subChoice);
+                    case 'a': {
+                        cout << "\n----- Tech Lead Login -----" << endl;
+                        cout << "Employee ID: ";
+                        cin >> employeeId;
+                        cout << "Password: ";
+                        cin >> password;
+
+                        if (loginTechLead(employeeId, password)) {
+                            cout << "Login successful. Welcome, Tech Lead" << endl;
+                            handleTechLeadSubMenu();
+                            // Handle Tech Lead submenu options
+                        } else {
+                            cout << "Invalid credentials. Login failed." << endl;
+                        }
                         break;
+}
                     case 'b':
-                        displayEngineeringManagerSubMenu();
-                        cin >> subChoice;
-                        handleEngineeringManagerSubMenuOption(subChoice);
+                        cout << "\n----- Engineering Manager Login -----" << endl;
+                        cout << "Employee ID: ";
+                        cin >> employeeId;
+                        cout << "Password: ";
+                        cin >> password;
+
+                        if (loginManager(employeeId, password)) {
+                            cout << "Login successful. Welcome, Manager" << endl;
+                            handleEMSubMenu();
+                            // Handle Tech Lead submenu options
+                        } else {
+                            cout << "Invalid credentials. Login failed." << endl;
+                        }
                         break;
                     case 'c':
-                        displayHumanResourcesSubMenu();
-                        cin >> subChoice;
-                        handleHumanResourcesSubMenuOption(subChoice);
-                        break;
-                    default:
-                        cout << "Invalid choice. Please try again." << endl;
+                        cout << "\n----- HR Login -----" << endl;
+                        cout << "Employee ID: ";
+                        cin >> employeeId;
+                        cout << "Password: ";
+                        cin >> password;
+
+                        if (loginManager(employeeId, password)) {
+                            cout << "Login successful. Welcome, HR" << endl;
+                            handleHRSubMenu();
+                            // Handle Tech Lead submenu options
+                        } else {
+                            cout << "Invalid credentials. Login failed." << endl;
+                        }
                         break;
                 }
                 
